99551f548864ea428132fa343556708fed969525
mehedixpressbd <mehedihasansk671@gmail.com>
2026-09-24 14:47:07 +0600

    Create firebase.js

---
+++ assets/css/assets/js/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import { firebaseConfig } from "../../firebase-config.js";
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
